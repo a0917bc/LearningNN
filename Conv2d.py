e91c484013f07ebb3ac11b08e51e5009d61ca5d1
Triven <a0917bc@gmail.com>
2021-04-20 16:13:23 +0800

    Learn from tutorial

--- Conv2d.py
+++ Conv2d.py
@@ -11,6 +11,12 @@ def processImage(image):
   image = cv2.imread(image) 
   image = cv2.cvtColor(src=image, code=cv2.COLOR_BGR2GRAY) 
   return image
+
+def paddingImage(image, padding=0):
+    if padding != 0:
+        imagePadded = np.zeros((image.shape[0] + padding*2, image.shape[1] + padding*2))
+        imagePadded[int(padding):int(-1 * padding), int(padding):int(-1 * padding)] = image
+    return imagePadded
 def convolve2D(image, kernel, padding=0, strides=1):
     # Cross Correlation
     kernel = np.flipud(np.fliplr(kernel))
@@ -28,22 +34,25 @@ def convolve2D(image, kernel, padding=0, strides=1):
 
     # Apply Equal Padding to All Sides
     if padding != 0:
-        imagePadded = np.zeros((image.shape[0] + padding*2, image.shape[1] + padding*2))
-        imagePadded[int(padding):int(-1 * padding), int(padding):int(-1 * padding)] = image
+        # imagePadded = np.zeros((image.shape[0] + padding*2, image.shape[1] + padding*2))
+        # imagePadded[int(padding):int(-1 * padding), int(padding):int(-1 * padding)] = image
+        
+        imagePadded = paddingImage(image, padding)
+        
         print(imagePadded)# Print 3S
     else:
         imagePadded = image
 
-    # Iterate through image
-    for y in range(image.shape[1]):
+    # Iterate through imagePadded
+    for y in range(imagePadded.shape[1]):
         # Exit Convolution
-        if y > image.shape[1] - yKernShape:
+        if y > imagePadded.shape[1] - yKernShape:
             break
         # Only Convolve if y has gone down by the specified Strides
         if y % strides == 0:
-            for x in range(image.shape[0]):
+            for x in range(imagePadded.shape[0]):
                 # Go to next row once kernel is out of bounds
-                if x > image.shape[0] - xKernShape:
+                if x > imagePadded.shape[0] - xKernShape:
                     break
                 try:
                     # Only Convolve if x has moved by the specified Strides
@@ -53,19 +62,52 @@ def convolve2D(image, kernel, padding=0, strides=1):
                 except:
                     break
 
+    # for y in range(image.shape[1]):
+    #     # Exit Convolution
+    #     # Only Convolve if y has gone down by the specified Strides
+    #     if y % strides == 0:
+    #         for x in range(image.shape[0]):
+    #             # Go to next row once kernel is out of bounds
+    #             if x % strides == 0:
+    #                     # numpy have dot operator so we don't need to another two for loop
+    #                     output[x, y] = (kernel * imagePadded[x:x +
+    #                      xKernShape, y: y + yKernShape]).sum()
+    
     return output
 # Just Conv2d operation, need to extend the channel form for CNN
 #%%
 #The driver code for conv2d is to do edge-detection
+# if __name__ == '__main__':
+#     # Grayscale Image
+#     image = processImage('Image.jpeg')
+
+#     # Edge Detection Kernel
+#     #kernel = np.array([[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]])
+#     kernel = np.ones((3, 3))
+#     kernel = kernel / 9
+    
+
+#     # Convolve and Save Output
+#     #output = convolve2D(image, kernel, padding=2)
+#     smoothed = convolve2D(image, kernel, padding=0)
+#     smoothed = paddingImage(smoothed, 1)
+#     detail = image - smoothed
+#     sharpened = image + detail
+#     cv2.imwrite('sharpenedImage.jpg', sharpened)
+#     #cv2.imwrite('2DConvolved.jpg', output)
+#%%
 if __name__ == '__main__':
-    # Grayscale Image
     image = processImage('Image.jpeg')
-
-    # Edge Detection Kernel
     kernel = np.array([[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]])
+    # imagePadded = paddingImage(image, padding=0)
+    output = convolve2D(image, kernel, padding=1)
+    # cv2.imwrite('2DConvolved.jpg', output)
+    #print(output)
+#%%
+kernel = np.array([[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]])
+A = np.arange(9).reshape(3, 3)
+A1 = paddingImage(A, padding=1)
+A2 = convolve2D(A1, kernel)
+#%%
 
-    # Convolve and Save Output
-    output = convolve2D(image, kernel, padding=2)
-    cv2.imwrite('2DConvolved.jpg', output)
 # %%
-#if __name__ == '__main__':
